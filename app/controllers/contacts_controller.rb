ab52763c4d864f9bc118f275043ecc4de370cd89
Arsalan <m.arsalan16723@gmail.com>
2016-07-18 13:42:31 +0500

    Added validations to form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,8 +5,21 @@ class ContactsController < ApplicationController
 	end
 
 	def create
+		@contact = Contact.new(contact_params)
 
-
+		if @contact.save
+			flash[:success] = "Message Sent"
+			redirect_to new_contact_path
+		else
+			flash[:danger] =  "Error.Could not send message"
+			render "new"
+		end
 	end
 
+
+	private
+		def contact_params
+			params.require(:contact).permit(:name, :email, :comments)
+
+		end
 end
